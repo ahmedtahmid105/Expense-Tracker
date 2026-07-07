8b0f8fe0cda2f4cc05f898727854913c5b7b01d5
Tahmid Ahmed <ahmedtahmid105@gmail.com>
2026-07-07 12:20:28 +0100

    Add files via upload

---
+++ expense_tracker.py
@@ -0,0 +1,8 @@
+print ("Welcome to the Daily Expense Tracker!")
+print ()
+print ("Menu:")
+print ("1. Add a new expense")
+print ("2. View all expenses")
+print ("3. Calculate total and average expense")
+print ("4. Clear all expenses")
+print ("5. Exit")
