b45c15a8f16b5bd590428e1febaee092f2577a94
Tahmid Ahmed <ahmedtahmid105@gmail.com>
2026-07-13 15:47:36 +0100

    Add files via upload

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,19 +1,16 @@
-print("Welcome to the Daily Expense Tracker!")
-
+print ("Welcome to the Daily Expense Tracker!")
 print("\nMenu:")
 print("1. Add a new expense")
 print("2. View all expenses")
 print("3. Calculate total and average expense")
 print("4. Clear all expenses")
 print("5. Exit")
-
-expense_list = None
-while True:
-    choice = int(input())
-
-    if choice == 5:
-        print("Exiting the Daily Expense Tracker. Goodbye!")
+expense_list = []
+while True :
+    choose_option = input()
+    if choose_option == "5" :
+        print ("Exiting the Daily Expense Tracker. Goodbye!")
         break
-    elif choice == 1 :
-        expense_list = float(input())
-        print ("Expense added successfully!")
+    elif choose_option == "1" :
+        expense_list = list(map(float,input().split()))
+        print("Expense added successfully!")
